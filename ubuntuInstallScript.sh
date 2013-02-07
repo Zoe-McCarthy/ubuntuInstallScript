c6298d0a5a1821f9e460f25333804bf45655eab9
Zoe McCarthy <zoemccarthy12@gmail.com>
2013-02-07 03:04:00 -0600

    added DRC stuff, fixed back to fuerte since DRC runits it(but didnt say it does!

--- ubuntuInstallScript.sh
+++ ubuntuInstallScript.sh
@@ -9,18 +9,25 @@ git clone https://github.com/Zoe-McCarthy/dotfiles.git
 
 #ROS stuff
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo sh -c 'echo "deb http://packages.osrfoundation.org/drc/ubuntu precise main" > /etc/apt/sources.list.d/drc-latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
+sudo apt-get update
+#echo "hddtemp hddtemp/daemon boolean false" | sudo debconf-set-selections # not sure what this is
+sudo apt-get install ros-fuerte-desktop-full
+
+# setup ros fuerte workspace
+sudo apt-get install python-rosinstall python-rosdep
+rosws init ~/fuerte_workspace /opt/ros/fuerte
+mkdir ~/fuerte_workspace/sandbox
+rosws set ~/fuerte_workspace/sandbox
+
+#echo "source /opt/ros/fuerte/setup.bash" >> ~/.bashrc
+#. ~/.bashrc
+
+# DRC simulator stuff
+sudo sh -c 'echo "deb http://packages.osrfoundation.org/drc/ubuntu precise main" > /etc/apt/sources.list.d/drc-latest.list'
 wget http://packages.osrfoundation.org/drc.key -O - | sudo apt-key add -
 sudo apt-get update
-echo "hddtemp hddtemp/daemon boolean false" | sudo debconf-set-selections
-sudo apt-get install ros-groovy-desktop-full
-sudo easy_install -U rosdep rospkg
-sudo rosdep init
-rosdep update
-echo "source /opt/ros/groovy/setup.bash" >> ~/.bashrc
-. ~/.bashrc
-sudo apt-get install python-rosinstall
+sudo apt-get install drcsim
 
 #sudo apt-get install ros-fuerte-openni-camera ros-fuerte-openni-launch
 
