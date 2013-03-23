fa2326ffb4d765e8d9a3b01b8953bda8002155a0
Zoe McCarthy <zoemccarthy12@gmail.com>
2013-03-22 20:27:15 -0700

    Fixed dotfiles

--- zoeDotfiles.sh
+++ zoeDotfiles.sh
@@ -2,5 +2,6 @@
 #vimrc/bashrc setup
 sudo apt-get install vim vim-gtk rake exuberant-ctags mercurial -y
 cd ~/
-git clone https://github.com/Zoe-McCarthy/dotfiles.git -y
-~/dotfiles/install.sh
+git clone https://github.com/Zoe-McCarthy/dotfiles.git
+cd dotfiles
+bash install.sh
